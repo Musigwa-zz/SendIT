0ccc17a6a469b3e954e42de795e8799088e13d02
Musigwa <pacifique.musigwa@gmail.com>
2018-12-22 21:05:20 +0200

    [Feature #16272279] fetch user parcels

--- UI/js/client/index.js
+++ UI/js/client/index.js
@@ -1,17 +1,20 @@
 const baseUrl = 'http://127.0.0.1:5070/api/v1/parcels';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imdsb3JpYUBnbWFpbC5jb20iLCJpZCI6MTcsImlzYWRtaW4iOmZhbHNlLCJpYXQiOjE1NDU0MDk1NzcsImV4cCI6MTU0NTQxMzE3N30.C4QX0OAjZPexsktGceFHtEFOKYiIGiz-fVPSNnckhEo';
+const loginUrl = '../../pages/auth/login.html';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imdsb3JpYUBnbWFpbC5jb20iLCJpZCI6MTcsImlzYWRtaW4iOmZhbHNlLCJpYXQiOjE1NDU1MDQ1NTEsImV4cCI6MTU0NTUwODE1MX0.krptk_Dm5o90i8i81Khc0L1C8cadST3JlsGZEpBfgNo';
 let toShow;
-
-const toggleToast = (message, expiresIn = 6000) => {
-  document.getElementById('toast-div').classList.add('show');
-  document.getElementById('toast-div').innerHTML = message;
+const toggleToast = (message, options = { expiresIn: 6000, type: 'warn' }) => {
+  const { expiresIn = 6000, type = 'warn' } = options;
+  let bgcolor = 'darkorange';
+  const toast = document.getElementById('toast-div');
+  if (type.includes('suc')) bgcolor = 'green';
+  else if (type.includes('er')) bgcolor = 'red';
+  toast.style.backgroundColor = bgcolor;
+  toast.classList.add('show');
+  toast.innerHTML = message;
   if (expiresIn !== false) {
-    document.getElementById('toast-div').style.animation = 'fadeInOut 6s';
-    document.getElementById('toast-div').classList.add('fade');
-    setTimeout(
-      () => document.getElementById('toast-div').classList.remove('show'),
-      expiresIn
-    );
+    toast.style.animation = `fadeInOut ${expiresIn / 1000}s`;
+    toast.classList.add('fade');
+    setTimeout(() => toast.classList.remove('show'), expiresIn);
   }
 };
 
@@ -26,81 +29,86 @@ fetch(baseUrl, {
 })
   .then(async res => {
     const body = await res.json();
+    const currency = 'Rwf';
+    const weightUnit = 'Kg';
     const { parcels = [], message = '' } = body;
     if (res.status === 200) {
-      const overview = [0, 0, 0];
+      toggleToast(message, { type: 'success', expiresIn: 3000 });
+      const overview = [0, 0, 0, 0];
       toShow = `
-          <div class="lg-card-content"><div class="data-table">
-          <table>
-            <tr>
-              <th>order id</th>
-              <th>recipient names</th>
-              <th>recipient phone</th>
-              <th>pickup location</th>
-              <th>current address</th>
-              <th>destination</th>
-              <th></th>
-              <th>weight</th>
-              <th>price</th>
-              <th>status</th>
-              <th>timestamp</th>
-              <th></th>
-            </tr>`;
+            <div class="lg-card-content"><div class="data-table">
+            <table>
+              <tr>
+                <th>order id</th>
+                <th>recipient names</th>
+                <th>recipient phone</th>
+                <th>pickup location</th>
+                <th>current address</th>
+                <th>destination</th>
+                <th></th>
+                <th>weight</th>
+                <th>price</th>
+                <th>status</th>
+                <th>date created</th>
+                <th></th>
+              </tr>`;
       parcels.forEach(parcel => {
         if (parcel.status.includes('unco')) overview[0]++;
-        else if (parcel.status.includes('deliv')) overview[1]++;
-        else if (parcel.status.includes('trans')) overview[2]++;
+        else if (parcel.status.includes('trans')) overview[1]++;
+        else if (parcel.status.includes('cancel')) overview[2]++;
+        else if (parcel.status.includes('deliv')) overview[3]++;
         const hidden = parcel.status.includes('cancel') || parcel.status.includes('deliv');
         toShow += `
-          <tr>
-            <td>${parcel.id}</td>
-            <td>${parcel.recipient_name}</td>
-            <td>${parcel.recipient_phone}</td>
-            <td>${parcel.origin}</td>
-            <td>${parcel.present_location}</td>
-            <td>${parcel.destination}</td>
-            <td>`;
+            <tr>
+              <td>${parcel.id}</td>
+              <td>${parcel.recipient_name}</td>
+              <td>${parcel.recipient_phone}</td>
+              <td>${parcel.origin}</td>
+              <td>${parcel.present_location}</td>
+              <td>${parcel.destination}</td>
+              <td>
+              `;
         toShow += hidden
           ? ''
-          : `
-              <img
+          : ` <img
                 onclick="openModal('edit-dest')"
                 class="hover"
                 src="../../assets/icons/edit.png"
                 alt="lg"
                 title="Change destination"
               />`;
-        toShow += `</td>
-            <td>${parcel.weight}</td>
-            <td>${parcel.price}</td>
-            <td>${parcel.status}</td>
-            <td>${new Date(parcel.createdat).toDateString()}</td>
-            <td>`;
+        toShow += `
+              </td>
+              <td>${parcel.weight} ${weightUnit}</td>
+              <td>${parcel.price} ${currency}</td>
+              <td>${parcel.status}</td>
+              <td>${new Date(parcel.createdat).toDateString()}</td>
+              <td>`;
         toShow += hidden
           ? ''
-          : `
-              <img
-                class="hover"
-                src="../../assets/icons/x-button.png"
-                alt="lg"
-                title="Cancel order"
-              />
+          : `<img
+              class="hover"
+              src="../../assets/icons/x-button.png"
+              alt="lg"
+              title="Cancel order"
+            />
             </td>
-          </tr>
-          `;
+            </tr>`;
       });
       toShow += '</table></div></div>';
       overview.forEach((value, index) => {
         document.getElementsByClassName('data').item(index).innerHTML = value;
       });
       document.getElementById('table-container').innerHTML = toShow;
-    } else if (res.status === 401) window.location = '../../pages/auth/login.html';
+    } else if (res.status === 401) window.location = loginUrl;
     else if (res.status === 404) toggleToast(`${message}. Use the bottom-right button to create one.`);
     else if (res.status === 500) {
-      toggleToast(
-        'Something went wrong! try again or contact the administrator.',
-        false
-      );
+      toggleToast('Something went wrong! try again or contact the administrator.', {
+        expiresIn: false
+      });
     }
   })
-  .catch(() => toggleToast('Something went wrong. Contact the administrator.', false));
+  .catch(() => toggleToast('Something went wrong. Contact the administrator.', {
+    type: 'error',
+    expiresIn: false
+  }));
